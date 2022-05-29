47267cc9af5391e2b376fd0b743b9829f2f43691
Jayvee Ballesteros <jayveeballesteros@gmail.com>
2022-05-29 11:44:36 +0800

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jayvee Ballesteros on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
